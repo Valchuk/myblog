5160324c2676b10334a8d5fdf2f2b142ad33e5fb
Valden <valden219@gmail.com>
2018-04-13 02:35:22 +0300

    Initial commit

---
+++ mysite/news/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Articles(models.Model):
+    title = models.CharField(max_length = 120)
+    post = models.TextField()
+    date = models.DateTimeField()
+    likes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
